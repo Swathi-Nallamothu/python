04b6a93d1c565011b85f424a13e95841c7e6e7d0
Swathi-Nallamothu <swathichinnarao1@gmail.com>
2025-05-28 10:48:40 +0530

    Add files via upload

---
+++ splitting.py
@@ -0,0 +1,6 @@
+a=input()
+print(a.split("a"))#it split the sentance based oon the mentioned
+print(a.partition("aa"))#divides the sentence into 3 parts based on mentioned word
+#1-before aa, 2-aa, 3-after aa
+print(a.rpartition("aa"))
+#1-upto aa, 2-second occurance of aa, 3-after aa
