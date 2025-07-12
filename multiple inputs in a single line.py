7813e627b2d6871ed2172d762a587e148442de7c
Swathi Nallamothu <swathichinnarao1@gmail.com>
2025-07-12 10:20:33 +0530

    Add files via upload

---
+++ multiple inputs in a single line.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+a=list(map(int,input().split()))
+b=map(int,input().split()) #it will not print the input
+c=tuple(map(int,input().split()))
+print(a)
+print(b)
+print(c)
+
+
+# In[ ]:
+
+
+
+
